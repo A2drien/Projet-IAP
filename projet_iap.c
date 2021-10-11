111e539663e9a91b699203ff0161910f1f25c2e3
Cyprien Méjat <adriendubois1411@gmail.com>
2021-10-11 13:18:50 +0200

    Ajout de commentaires

--- projet_iap.c
+++ projet_iap.c
@@ -3,7 +3,7 @@ const unsigned int nbMatch = 127;           // nb de matchs par tournois
 const unsigned int nbJoueuses = 128;        // nb joueuses par tournois
 const unsigned int lgMot = 30;              // nb de caractères max
 
-char mot[lgMot+1];                          // chaîne de caractères (mot) de taille max lgMot
+char mot[lgMot+1];                          // chaîne de caractères (mot) de taille max lgMot contenant la commande
 
 typedef struct{
     unsigned char nomJoueuse;               // nom de la joueuse
@@ -22,17 +22,18 @@ typedef struct{
 }Tournoi;
 
 typedef struct{
-    Tournoi tableauTournois[maxTournois+1];   // tableau des tournois
+    Tournoi tableauTournois[maxTournois+1]; // tableau des tournois
 }TournoisWTA;
 
 
 int main(){
     while(1){
-        scanf("%c", mot);
-        commande(&mot);
+        scanf("%c", mot);                   // entrée de la commande
+        commande(&mot);                     // analyse de la commande
     }
 }
 
+// Analyse de la commande entrée
 char commande(char* mot){
     char text;
     for (int i=0; i<lgMot; i++){
