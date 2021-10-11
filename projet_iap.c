e95056289f26d411fa8f0149c4ec8e28f91d41fa
Cyprien Méjat <52292872+A2drien@users.noreply.github.com>
2021-10-11 12:54:17 +0200

    Add files via upload

---
+++ projet_iap.c
@@ -0,0 +1,50 @@
+const unsigned int maxTournois = 10;        // nb maximum de tournois
+const unsigned int nbMatch = 127;           // nb de matchs par tournois
+const unsigned int nbJoueuses = 128;        // nb joueuses par tournois
+const unsigned int lgMot = 30;              // nb de caractères max
+
+char mot[lgMot+1];                          // chaîne de caractères (mot) de taille max lgMot
+
+typedef struct{
+    unsigned char nomJoueuse;               // nom de la joueuse
+    int pointsCummules;                     // nb points cummulés
+}Joueuse;
+
+typedef struct{
+    unsigned int indexGagnante;             // index de la gagnante
+    unsigned int indexPerdante;             // index de la perdante
+}Match;
+
+typedef struct{
+    unsigned char nomTournoi;               // nom du tournoi
+    unsigned int date;                      // date du tournoi
+    Match tableauMatchs[nbMatch+1];         // tableau des 127 matchs
+}Tournoi;
+
+typedef struct{
+    Tournoi tableauTournois[maxTournois+1];   // tableau des tournois
+}TournoisWTA;
+
+
+int main(){
+    while(1){
+        scanf("%c", mot);
+        commande(&mot);
+    }
+}
+
+char commande(char* mot){
+    char text;
+    for (int i=0; i<lgMot; i++){
+        text += mot[i];
+        if (text == "definir_nombre_tournois"){
+            return "qlq";
+        }
+
+        else if (text == "enregistrement_tournoi"){
+            
+        }
+        
+    }
+}
+
